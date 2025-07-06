29a449618b5a14c5d372e0062ab4dab4490a5664
Yash Chandra Verma <ycverma005@gmail.com>
2025-07-06 10:53:08 +0530

    Update AndroidPlayInstallReferrerPlugin.kt

--- android/src/main/kotlin/de/lschmierer/android_play_install_referrer/AndroidPlayInstallReferrerPlugin.kt
+++ android/src/main/kotlin/de/lschmierer/android_play_install_referrer/AndroidPlayInstallReferrerPlugin.kt
@@ -1,5 +1,5 @@
 package de.lschmierer.android_play_install_referrer
- 
+
 import android.content.Context
 import androidx.annotation.NonNull
 import com.android.installreferrer.api.InstallReferrerClient
@@ -10,9 +10,9 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import java.rmi.RemoteException
+import android.os.RemoteException // Correct import
 import kotlin.collections.ArrayList
- 
+
 /** AndroidPlayInstallReferrerPlugin */
 class AndroidPlayInstallReferrerPlugin : FlutterPlugin, MethodCallHandler {
     private lateinit var context: Context
@@ -21,19 +21,19 @@ class AndroidPlayInstallReferrerPlugin : FlutterPlugin, MethodCallHandler {
     private var referrerClient: InstallReferrerClient? = null
     private var referrerDetails: ReferrerDetails? = null
     private var referrerError: Pair<String, String>? = null
- 
+
     private val isInstallReferrerPending: Boolean
         @Synchronized
         get() {
             return referrerClient != null && !isInstallReferrerResolved
         }
- 
+
     private val isInstallReferrerResolved: Boolean
         @Synchronized
         get() {
             return referrerDetails != null || referrerError != null
         }
- 
+
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         this.context = flutterPluginBinding.applicationContext
         channel = MethodChannel(
@@ -42,7 +42,7 @@ class AndroidPlayInstallReferrerPlugin : FlutterPlugin, MethodCallHandler {
         )
         channel.setMethodCallHandler(this)
     }
- 
+
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
         if (call.method == "getInstallReferrer") {
             getInstallReferrer(result)
@@ -50,34 +50,34 @@ class AndroidPlayInstallReferrerPlugin : FlutterPlugin, MethodCallHandler {
             result.notImplemented()
         }
     }
- 
+
     @Synchronized
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         pendingResults.clear()
         referrerClient?.endConnection()
         channel.setMethodCallHandler(null)
     }
- 
+
     @Synchronized
     private fun getInstallReferrer(@NonNull result: Result) {
         if (isInstallReferrerResolved) {
             resolveInstallReferrerResult(result)
         } else {
             pendingResults.add(result)
- 
+
             if (!isInstallReferrerPending) {
                 referrerClient = InstallReferrerClient.newBuilder(context).build()
                 referrerClient?.startConnection(object : InstallReferrerStateListener {
                     override fun onInstallReferrerSetupFinished(responseCode: Int) {
                         handleOnInstallReferrerSetupFinished(responseCode)
                     }
- 
+
                     override fun onInstallReferrerServiceDisconnected() {}
                 })
             }
         }
     }
- 
+
     @Synchronized
     private fun handleOnInstallReferrerSetupFinished(responseCode: Int) {
         when (responseCode) {
@@ -96,43 +96,43 @@ class AndroidPlayInstallReferrerPlugin : FlutterPlugin, MethodCallHandler {
                     referrerError = Pair("BAD_STATE", "Referrer client is null.")
                 }
             }
- 
+
             InstallReferrerClient.InstallReferrerResponse.SERVICE_DISCONNECTED -> {
                 referrerError = Pair(
                     "SERVICE_DISCONNECTED",
                     "Play Store service is not connected now - potentially transient state."
                 )
             }
- 
+
             InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE -> {
                 referrerError = Pair("SERVICE_UNAVAILABLE", "Connection couldn't be established.")
             }
- 
+
             InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED -> {
                 referrerError = Pair(
                     "FEATURE_NOT_SUPPORTED",
                     "API not available on the current Play Store app."
                 )
             }
- 
+
             InstallReferrerClient.InstallReferrerResponse.DEVELOPER_ERROR -> {
                 referrerError = Pair("DEVELOPER_ERROR", "General errors caused by incorrect usage.")
             }
- 
+
             InstallReferrerClient.InstallReferrerResponse.PERMISSION_ERROR -> {
                 referrerError =
                     Pair("PERMISSION_ERROR", "App is not allowed to bind to the Service.")
             }
- 
+
             else -> {
                 referrerError =
                     Pair("UNKNOWN_ERROR", "InstallReferrerClient returned unknown response code.")
             }
         }
- 
+
         resolvePendingInstallReferrerResults()
     }
- 
+
     @Synchronized
     private fun resolvePendingInstallReferrerResults() {
         pendingResults.forEach {
@@ -140,7 +140,7 @@ class AndroidPlayInstallReferrerPlugin : FlutterPlugin, MethodCallHandler {
         }
         pendingResults.clear()
     }
- 
+
     @Synchronized
     private fun resolveInstallReferrerResult(@NonNull result: Result) {
         referrerDetails?.let { details ->
